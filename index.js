bea23e015b30d9d830bcdbf258a82d261833d80c
Akilah Snowden <akilahdsnowden@gmail.com>
2022-04-26 11:32:49 -0400

    commit tree 0ec2016cb07e51dfa675cedc3231d396c093087e

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+/*
+distanceFromHqInBlocks()
+       returns a distance in blocks
+       calculates distances below 42nd street
+*/
+function distanceFromHqInBlocks(blockValue){
+    const scuberBlock = 42; //declare scubers block address 
+    return (Math.abs(scuberBlock - blockValue) //Math.abs()returns absolute value of number
+)}
+
+//function returns distanceFromHqInFeet, calls distanceFromHqInBlocks function
+//each block = 264 feet
+//
+function distanceFromHqInFeet(feetValue){
+   return (distanceFromHqInBlocks(feetValue) * 264)
+}
+
+//Function that returns distance traveled in feet includes(start, destination) parameters
+//add absolute value difference of start and destination (in ft)
+//multiply difference by 264
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264                                              
+}
+
+/*function calculates fare price given start and destination parameters 
+    between 400 and 2000 ft price is 2 cents/ft
+    flat rate fare for distance over 2000 and less than 2500
+    over 2500 ft -> 'cannot travel that far'
+*/
+function calculatesFarePrice(start, destination){
+    const num1 = distanceTravelledInFeet(start, destination)
+    if(num1 <= 400){
+        return 0
+    } else if(num1 > 400 && num1 < 2000){
+        return(num1-400) * .02 //first 400ft are free, .02 cents/foot
+    } else if(num1 >= 2000 && num1 < 2500){
+        return 25
+    } else (num1 >= 2500)
+        return 'cannot travel that far'
+    }
--- test/indexTest.js
+++ test/indexTest.js
@@ -46,8 +46,8 @@ describe('index.js', function() {
       expect(calculatesFarePrice(43, 44)).to.equal(0);
     });
 
-    it('charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!)', function() {
-      // total feet travelled is 528, minus the first 400 feet, comes to 128 -> 2.56
+    it('charges 2 cents per foot when total feet traveled is between 400 and 2000 (remember the first 400 feet are free!)', function() {
+      // total feet traveled is 528, minus the first 400 feet, comes to 128 -> 2.56
       expect(calculatesFarePrice(34, 32)).to.equal(2.56);
     });
 
